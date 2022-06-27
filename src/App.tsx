7b777ddf026fae290f98a567da935c0ab4582da1
Robert DeAngelis <deangelis.robert13@gmail.com>
2022-06-26 21:13:21 -0400

    to github nowgit add .git add .!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import InputFeild from "./components/InputFeild";
+import TodoList from "./components/TodoList";
+import {Todo} from "./model"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todo, setTodo] = useState<string>("");
+    const [todos,setTodos] = useState<Todo[]>([]);
+
+    const handleAdd = (e: React.FormEvent) => {
+        e.preventDefault();
+        if(todo){
+            setTodos([...todos,{id:Date.now(),todo:todo,isDone:false}]);
+            setTodo("");
+        }
+    };
+
+    console.log(todo);
+    console.log(todos);
+
+    return (
+        <div className="App">
+            <span className="heading">Zettle</span>
+            <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+            <TodoList todos={todos} setTodos={setTodos}/>
+
+        </div>
+        );
 }
 
 export default App;
